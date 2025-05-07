bb993dfb384072d7cbbaeef82198342b7a454f90
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-05-06 22:44:18 -0400

    diff: submission for codepath

--- index.js
+++ index.js
@@ -29,22 +29,28 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
         // create a new div element, which will become the game card
+        const gameCard = document.createElement("div");
+        gameCard.className = "game-card";
+        gameCard.id = "game";
 
 
         // add the class game-card to the list
+        games[i] = gameCard;
 
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        document.getElementByID("game").innerHTML = "
+            <img></img>
+            ";
 
 
         // append the game to the games-container
-
+    }
 }
 
 // call the function we just defined using the correct variable
